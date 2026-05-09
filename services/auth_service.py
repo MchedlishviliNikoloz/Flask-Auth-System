858cf6bb8f856dabc11d34d3b36fe456461969f4
MchedlishviliNikoloz <mchedlishvili.creative@gmail.com>
2026-05-09 13:11:59 +0400

    feat: add auth service (register, authenticate...)

---
+++ services/auth_service.py
@@ -0,0 +1,70 @@
+from models.user import User
+from database import db
+from utils.validators import validate_username, validate_email, validate_password
+
+
+def email_exists(email: str) -> bool:
+    email_check = db.session.execute(db.select(User).filter_by(email=email)).scalar() is not None
+    return email_check
+
+def username_exists(username: str) -> bool:
+    username_check = db.session.execute(db.select(User).filter_by(username=username)).scalar() is not None
+    return username_check
+
+def get_user_by_username(username: str) -> User:
+    return db.session.execute(db.select(User).filter_by(username=username)).scalar_one_or_none()
+
+def get_user_by_email(email: str) -> User:
+    return db.session.execute(db.select(User).filter_by(email=email)).scalar_one_or_none()
+
+def get_user_by_id(user_id: int) -> User:
+    return db.session.execute(db.select(User).filter_by(id=user_id)).scalar_one_or_none()
+
+def detect_login_input(login_input: str) -> str:
+    if "@" in login_input:
+        return "email"
+    return "username"
+
+def register_user(user: User):
+    if username_exists(user.username):
+        return {"success": False, "errors": ["Username already exists."],}
+    if email_exists(user.email):
+        return {"success": False, "errors": ["Email already exists."],}
+
+    username_validation = validate_username(user.username)
+    email_validation = validate_email(user.email)
+    password_validation = validate_password(user.password, user.username)
+
+    if not username_validation["success"]:
+        return {"success": False, "errors": username_validation["errors"],}
+
+    if not email_validation["success"]:
+        return {"success": False, "errors": email_validation["errors"],}
+
+    if not password_validation["success"]:
+        return {"success": False, "errors": password_validation["errors"],}
+
+    db.session.add(user)
+    db.session.commit()
+    return {"success": True, "user": user}
+
+def authenticate_user(login_input: str, password: str):
+    login_input_type = detect_login_input(login_input)
+    if login_input_type == "email":
+        user = get_user_by_email(login_input)
+        if not user:
+            return {"success": False, "errors": ["Email or Password is incorrect."],}
+
+        if not user.check_password(password):
+            return {"success": False, "errors": ["Email or Password is incorrect."],}
+
+        return {"success": True, "user": user}
+
+    user = get_user_by_username(login_input)
+    if not user:
+        return {"success": False, "errors": ["Username or Password is incorrect."], }
+
+    if not user.check_password(password):
+        return {"success": False, "errors": ["Username or Password is incorrect."], }
+
+    return {"success": True, "user": user}
