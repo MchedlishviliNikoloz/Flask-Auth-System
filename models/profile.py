1bee773c8b1d20d8febb0a2f4e0fd24c3fe2107b
MchedlishviliNikoloz <mchedlishvili.creative@gmail.com>
2026-05-09 18:55:38 +0400

    feat: add Profile model with foreign key to User

---
+++ models/profile.py
@@ -0,0 +1,31 @@
+from database import db
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime, timezone
+
+
+class Profile(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"),
+        nullable=False,
+        unique=True
+    )
+    user: Mapped["User"] = relationship(back_populates="profile")
+    first_name: Mapped[str] = mapped_column(nullable=True)
+    last_name: Mapped[str] = mapped_column(nullable=True)
+    bio: Mapped[str] = mapped_column(nullable=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(
+        default=datetime.now(timezone.utc),
+        onupdate=datetime.now(timezone.utc)
+    )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "bio": self.bio
+        }
