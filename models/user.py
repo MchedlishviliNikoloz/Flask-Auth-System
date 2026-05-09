179aa48c1bcbc6b0d1f9307ca505aecc62e95f69
MchedlishviliNikoloz <mchedlishvili.creative@gmail.com>
2026-05-09 18:55:52 +0400

    refactor: remove first_name and last_name from User, add relationship to Profile

--- models/user.py
+++ models/user.py
@@ -1,12 +1,14 @@
 from database import db
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime, timezone
+
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    profile: Mapped["Profile"] = relationship(
+        back_populates="user", uselist=False
+    )
     username: Mapped[str] = mapped_column(unique=True, nullable=False)
-    first_name: Mapped[str] = mapped_column(nullable=True)
-    last_name: Mapped[str] = mapped_column(nullable=True)
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     created_at: Mapped[datetime] = mapped_column(default=datetime.now(timezone.utc))
@@ -15,8 +17,6 @@ class User(db.Model):
         return {
             "id": self.id,
             "username": self.username,
-            "first_name": self.first_name,
-            "last_name": self.last_name,
             "email": self.email
         }
 
